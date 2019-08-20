0dd396731457678013ecd508c8ae04392e5e8d53
Mario Renteria <mrente94@gmail.com>
2019-08-20 14:01:46 -0700

    starter code

---
+++ db/schema.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers(
+    id int auto_increment,
+    burger_name varchar(100),
+    devoured boolean 
+);
+
---
+++ db/seeds.sql
@@ -0,0 +1,2 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Triple Meat Triple Cheese", false), ("Double Meat Double Cheese", false), ("Mushroom Burger", false);
